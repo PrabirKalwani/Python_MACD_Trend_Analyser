b1d4177a0a8af792733be7ecfe874754060cac8c
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-04-09 16:23:45 +0530

    Working Html css added

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,4 +1,4 @@
-"""stocks URL Configuration
+"""myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-from django.urls import include, path
+    path('', home, name='home'),
 
-urlpatterns = [
-    path('', include('main.urls')),
 ]
+
