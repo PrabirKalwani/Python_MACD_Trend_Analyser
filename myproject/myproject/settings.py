30ed68c9ec00e50dab5720e01a8d1b4f3ebe0600
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-04-09 22:36:57 +0530

    Home page done

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,7 +122,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'website/static']
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'website/media/')
+MEDIA_URL = '/media/'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from website.views import home, learn, predict
@@ -23,3 +25,5 @@ urlpatterns = [
     path('learn/', learn, name='learn'),
     path('predict/', predict, name='predict'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
