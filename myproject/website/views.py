35d15fb766a2609bbb7a810602e35ba1770e4dff
aayushshah1 <126041880+aayushshah1@users.noreply.github.com>
2023-04-14 01:01:31 +0530

    routing of learn files
    routed multiple files

--- myproject/website/views.py
+++ myproject/website/views.py
@@ -10,4 +10,33 @@ def learn(request):
 def predict(request):
     return render(request, 'website/predict.html')
 
+def penant(request):
+    return render(request, 'website/learn/penant.html')
+
+def flag(request):
+    return render(request, 'website/learn/flag.html')
+
+def wedge(request):
+    return render(request, 'website/learn/wedge.html')
+
+def ascending(request):
+    return render(request, 'website/learn/ascending.html')
+
+def descending(request):
+    return render(request, 'website/learn/descending.html')
+
+def symmetrical(request):
+    return render(request, 'website/learn/symmetrical.html')
+
+def cup(request):
+    return render(request, 'website/learn/cup.html')
+
+def head(request):
+    return render(request, 'website/learn/head.html')
+
+def double(request):
+    return render(request, 'website/learn/double.html')
+
+def gaps(request):
+    return render(request, 'website/learn/gaps.html')
 
